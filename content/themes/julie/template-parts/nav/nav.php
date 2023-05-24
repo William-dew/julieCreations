7a36a2c4cee05db71f2df6af84e8379cf16bdb9d
William-dew <william.dewailly@gmail.com>
2023-05-24 21:38:27 +0100

    description front-page

--- content/themes/julie/template-parts/nav/nav.php
+++ content/themes/julie/template-parts/nav/nav.php
@@ -1,7 +1,7 @@
 <nav class="menu-top">
     <ul>
         <li><a href="#">Accueil</a></li>
-        <li><a href="#">Mes Tableaux</a></li>
+        <li><a href="#">œuvres</a></li>
         <li><a href="#">À propos de moi</a></li>
         <li><a href="#">Contact</a></li>
     </ul>

bf817e65974364a6e3a5f5d3bbce57139fca7cb6
William-dew <william.dewailly@gmail.com>
2023-07-28 08:33:46 +0100

    add 404 page

---
+++ content/themes/julie/404.php
@@ -0,0 +1,25 @@
+<?php get_header(); ?>
+<div class="container page_404 text-center">
+    <h1 class="h1 display-4 m-3">Erreur 404 - Œuvre introuvable</h1>
+    <p class="fs-4">
+
+        Bienvenue dans mon univers artistique, où les couleurs et les émotions s'entrelacent pour créer des tableaux uniques. Malheureusement, vous avez atteint une impasse sur la toile virtuelle.
+
+        Tel un pinceau qui glisse sur la toile, je donne vie à mes rêves et à mes inspirations les plus profondes. Chaque coup de pinceau est une histoire racontée en nuances, chaque touche une émotion capturée dans le temps.
+
+        Mais parfois, même les plus belles créations peuvent se cacher dans les méandres de l'inconnu. Et c'est ici que vous vous trouvez : sur une page vierge, en quête de l'œuvre qui vous a attiré.
+
+        Ne soyez pas déçu ! Tel un véritable explorateur, laissez-vous guider par la curiosité et continuez votre recherche au sein de mes galeries. Peut-être que votre découverte vous attend juste au détour d'un clic.
+
+        En attendant, prenez le temps d'apprécier les détails subtils qui font la richesse de cette page 404. Elle est le reflet de mon monde créatif, où chaque instant d'émerveillement est précieux.
+
+        N'hésitez pas à revenir sur vos pas en utilisant les liens de navigation, ou à me contacter directement si vous avez besoin d'aide pour retrouver le chemin vers l'une de mes œuvres.
+
+
+        L'art est un voyage sans fin, où chaque tournant offre de nouvelles perspectives. Je vous remercie de votre visite et vous souhaite une agréable exploration dans cet univers artistique qui m'anime.
+
+        <br>Bien à vous,<br>Julie Jousset</p>
+  
+
+</div>
+<?php get_footer(); ?>
